e8ab56c509ee500772e9fa9cf6954f3350e5cc9e
Francisco Souza <francisco@franciscosouza.net>
2011-04-08 23:55:30 -0300

    Doing the raffle

--- raffle.py
+++ raffle.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import android
 import random
 
 class Raffler(object):
@@ -15,3 +16,9 @@ def notify_raffled_number(droid, number):
 
 def ask_maximum_number(droid):
     return int(droid.dialogGetResponse("Número máximo", "Qual o número de inscritos?").result)
+
+if __name__ == '__main__':
+    droid = android.Android()
+    max_number = ask_maximum_number(droid)
+    raffled_number = Raffler(min=1, max=max_number).raffle()
+    notify_raffled_number(droid, raffled_number)
